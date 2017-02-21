40a7992e11fd591e38ddcc9326edf2c30761add4
andrew1007 <yueh.andrew@gmail.com>
2017-02-20 16:56:46 -0800

    nn maintenance

--- svmOpt_v2.R
+++
@@ -1,3 +0,0 @@
-SVMOpt <- function(data_csv, test_data, kernel = 'polynomial', dimensions = 4, samplesize_percentage = 0.85, input_cols, output_cols, sim_count = 10, cross_validate_count = 100){
-  raw_data <- read.csv(data_csv)
-}
